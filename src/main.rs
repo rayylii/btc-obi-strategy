a33292f03fc1810200bf3f062e9dade8b0468c0d
rayylii <rligk1@hotmail.com>
2026-06-12 16:54:10 +0100

    add l2 and order book imbalance signal

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,72 @@
 use futures_util::StreamExt;
 use serde::Deserialize;
 use tokio::signal;
-use tokio_tungstenite::{
-    connect_async, tungstenite::Message, tungstenite::client::IntoClientRequest,
-};
+use tokio_tungstenite::connect_async;
+use tokio_tungstenite::tungstenite::Message;
+use tokio_tungstenite::tungstenite::client::IntoClientRequest;
 
-#[derive(Deserialize, Debug)]
-struct Quote {
-    #[serde(rename = "b")]
-    best_bid_price: String,
-
-    #[serde(rename = "B")]
-    best_bid_qty: String,
-
-    #[serde(rename = "a")]
-    best_ask_price: String,
-
-    #[serde(rename = "A")]
-    best_ask_qty: String,
+#[derive(Debug, Deserialize)]
+struct Quotes {
+    bids: Vec<(String, String)>,
+    asks: Vec<(String, String)>,
 }
 
 #[derive(Debug)]
 struct OrderBook {
-    best_bid_price: f64,
-    best_bid_qty: f64,
-    best_ask_price: f64,
-    best_ask_qty: f64,
+    bids: Vec<(f64, f64)>,
+    asks: Vec<(f64, f64)>,
     spread: f64,
+    order_book_imbalance: f64,
 }
 
 impl OrderBook {
     pub fn new() -> Self {
-        Self {
-            best_bid_price: 0.0,
-            best_bid_qty: 0.0,
-            best_ask_price: 0.0,
-            best_ask_qty: 0.0,
+        OrderBook {
+            bids: Vec::new(),
+            asks: Vec::new(),
             spread: 0.0,
+            order_book_imbalance: 0.0,
         }
     }
 
-    pub fn update(&mut self, quote: Quote) {
-        self.best_bid_price = quote.best_bid_price.parse::<f64>().unwrap();
-        self.best_bid_qty = quote.best_bid_qty.parse::<f64>().unwrap();
-        self.best_ask_price = quote.best_ask_price.parse::<f64>().unwrap();
-        self.best_ask_qty = quote.best_ask_qty.parse::<f64>().unwrap();
-        self.spread = self.best_ask_price - self.best_bid_price;
+    pub fn update(&mut self, quotes: Quotes) {
+        self.bids = quotes
+            .bids
+            .into_iter()
+            .map(|(price, qty)| (price.parse::<f64>().unwrap(), qty.parse::<f64>().unwrap()))
+            .collect::<Vec<(f64, f64)>>();
+        self.asks = quotes
+            .asks
+            .into_iter()
+            .map(|(price, qty)| (price.parse::<f64>().unwrap(), qty.parse::<f64>().unwrap()))
+            .collect::<Vec<(f64, f64)>>();
+        self.spread = self.asks.first().unwrap().0 - self.bids.first().unwrap().0;
+        let bid_volume = self.bids.iter().map(|(_price, qty)| qty).sum::<f64>();
+        let ask_volume = self.asks.iter().map(|(_price, qty)| qty).sum::<f64>();
+        self.order_book_imbalance = bid_volume / (bid_volume + ask_volume);
     }
 
     pub fn display(&self) {
+        println!("-");
+        for (price, qty) in self.bids.iter() {
+            println!("bid: {}, qty: {}", price, qty);
+        }
         println!(
-            "-\nbid: {}, qty: {}\nask: {}, qty: {}\nspread: {}",
-            self.best_bid_price,
-            self.best_bid_qty,
-            self.best_ask_price,
-            self.best_ask_qty,
-            self.spread,
-        )
+            "-\nspread: {}\norder book imbalance: {}\n-",
+            self.spread, self.order_book_imbalance
+        );
+        for (price, qty) in self.asks.iter().rev() {
+            println!("ask: {}, qty: {}", price, qty);
+        }
     }
 }
 
 #[tokio::main]
 async fn main() {
-    let request = "wss://stream.binance.com:9443/ws/btcusdt@bookTicker"
+    let request = "wss://stream.binance.com:9443/ws/btcusdt@depth10"
         .into_client_request()
         .unwrap();
+
     let (mut ws_stream, _) = connect_async(request).await.unwrap();
     let mut order_book = OrderBook::new();
 
@@ -72,13 +74,15 @@ async fn main() {
         tokio::select! {
             _ = signal::ctrl_c() => {
                 println!("-\nshutting down");
-                break; },
+                break;
+            }
             msg = ws_stream.next() => {
                 if let Some(Ok(Message::Text(text))) = msg {
-                    let quote: Quote = serde_json::from_str(&text).unwrap();
+                    let quotes: Quotes = serde_json::from_str(&text).unwrap();
 
-                    order_book.update(quote);
+                    order_book.update(quotes);
                     order_book.display();
+
                 }
             }
         }
